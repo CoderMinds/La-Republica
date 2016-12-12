33dc6f825d0b4941ee5148fb31cb3f851441ea5f
Maricela Alvarez <malvarezdrobush@gmail.com>
2016-12-12 16:29:30 -0500

    navegacion carrousel anadida

---
+++ js/slick.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+      $('.temas-dia').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        variableWidth: true,
+      });
+    });
